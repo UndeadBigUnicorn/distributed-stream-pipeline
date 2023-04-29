cd28e2564325d40d408f8862f8c984ed467f1d5b
Danylo Kravchenko <kravchel16@gmail.com>
2023-04-29 14:30:09 +0200

    changed driver port fo the statistics stream

--- src/spark/statistics/run_job.sh
+++ src/spark/statistics/run_job.sh
@@ -2,4 +2,4 @@
 
 pip install numpy
 
-/opt/bitnami/spark/bin/spark-submit --conf spark.driver.port=9100 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2 --master local[2] --executor-memory 8G --num-executors 2 --name StatisticsStream statistics_stream.py
+/opt/bitnami/spark/bin/spark-submit --conf spark.driver.port=10099 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2 --master local[2] --executor-memory 8G --num-executors 2 --name StatisticsStream statistics_stream.py
